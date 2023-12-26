9d09f983ced3e3b4dd3adec273149d1af9679ae8
shreyansh <shreyansh@umass.edu>
2023-12-26 02:10:31 -0500

    Content update

--- src/app/research/page.tsx
+++ src/app/research/page.tsx
@@ -11,15 +11,19 @@ export default async function ResearchPage() {
   return (
     <section>
       <p className="text-[35px] text-black dark:text-white font-extrabold tracking-widest mb-6">
-        Research
+        Research Profile
       </p>
+      
+      <p className="leading-loose mt-8">
+        <span className="font-bold">Interests:</span> I'm interested in data
+        science and machine learning, especially when applied to areas like
+        public health, sports analytics, and the social sciences.
+      </p>
+
       <ul className="flex flex-col md:flex-row mt-8 space-x-0 md:space-x-4 space-y-2 md:space-y-0 font-sm text-neutral-600 dark:text-neutral-300">
         <p className="text-white font-bold">Lab Trainings</p>:
         <TextLink text="Lab Safety" link="./certificates/labsafety.pdf" />
-        <TextLink
-          text="Fire Safety"
-          link="./certificates/firesafety.pdf"
-        />
+        <TextLink text="Fire Safety" link="./certificates/firesafety.pdf" />
         <TextLink
           text="Bloodborne Pathogens"
           link="./certificates/bloodbornepathogens.pdf"
@@ -39,9 +43,10 @@ export default async function ResearchPage() {
       </ul>
 
       <p className="leading-loose mt-8">
-        <span className="font-bold">Relevant Coursework</span>: Programming with Data Structures, Computer Systems, Intro to
-        Computation (Discrete Mathematics), Multivariate Calculus, Linear
-        Algebra, Intro to Kinesiology.
+        <span className="font-bold">Relevant Coursework</span>: Programming with
+        Data Structures, Computer Systems, Intro to Computation (Discrete
+        Mathematics), Multivariate Calculus, Linear Algebra, Intro to
+        Kinesiology.
       </p>
     </section>
   );
