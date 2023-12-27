19de408c394f321f71447b9d2a5e0bc0c45af756
shreyansh <shreyansh@umass.edu>
2023-12-27 11:58:30 -0500

    Test

--- src/app/more/page.tsx
+++ src/app/more/page.tsx
@@ -9,9 +9,12 @@ export const metadata: Metadata = {
 export default async function MorePage() {
   return (
     <section>
+      <script src="https://platform.linkedin.com/badges/js/profile.js" async defer type="text/javascript"></script>
       <p className="text-[35px] text-black dark:text-white font-extrabold tracking-widest mb-6">
         Coming soon
       </p>
+
+      <div class="badge-base LI-profile-badge" data-locale="en_US" data-size="medium" data-theme="dark" data-type="VERTICAL" data-vanity="shreyanshmisra" data-version="v1"><a class="badge-base__link LI-simple-link" href="https://www.linkedin.com/in/shreyanshmisra?trk=profile-badge">Shreyansh M.</a></div>
     </section>
   );
 }
